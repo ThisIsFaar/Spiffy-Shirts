f2bbb4a81ba6b2702715137408161e928d499657
thisisfaar <thisisfarhan99@gmail.com>
2021-12-04 00:26:35 +0530

    a

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use("/api", orderRoutes);
 app.use("/api", stripeRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("frontend/src"));
 }
 
 //Starting a Server
