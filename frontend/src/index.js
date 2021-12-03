b6167293de3a13a0e449c0c04ffb7fc07f3fa5d8
thisisfaar <thisisfarhan99@gmail.com>
2021-12-03 21:45:47 +0530

    pwa

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Routes from "./Routes";
 import ReactDOM from "react-dom";
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 ReactDOM.render(<Routes />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
